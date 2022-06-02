ffcd7ab575d65ebe3d62ef5671cb443609b980b5
forhadx <forhadsh1@gmail.com>
2022-06-03 00:41:06 +0600

    add a counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    let localVal = localStorage.getItem("val");
+    if (localVal !== null) {
+      setValue(+localVal);
+    }
+  }, []);
+
+  const negtiveHandler = () => {
+    setValue(value - 1);
+    localStorage.setItem("val", value - 1);
+  };
+
+  const positiveHandler = () => {
+    setValue(value + 1);
+    localStorage.setItem("val", value + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Count: <span>{value}</span>
+        </h1>
+        <div>
+          <button className="m-btn" onClick={negtiveHandler}>
+            -
+          </button>
+          <button onClick={positiveHandler}>+</button>
+        </div>
       </header>
     </div>
   );
